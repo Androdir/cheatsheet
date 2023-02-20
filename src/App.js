20f4bd8fdd20a27fffbca5958b49a1c90d54b218
Androdir <66904930+Androdir@users.noreply.github.com>
2023-02-20 22:21:19 +0100

    added mathjax

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { MathJax } from 'better-react-mathjax';
 import './App.css';
 
 function App() {
@@ -5,6 +6,8 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<h4>helo :smiley:</h4>
+				<MathJax>{"\\(\\frac{10}{4x} \\approx 2^{12}\\)"}</MathJax>
+
 			</header>
 		</div>
 	);
